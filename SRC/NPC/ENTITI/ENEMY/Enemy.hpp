21d3b8250dba91a2ddb9ff30d29439e04a66f46f
CMIK-slow <megabookZEQ@outlook.com>
2026-02-15 17:13:44 +0300

    Update Enemy.hpp

--- SRC/NPC/ENTITI/ENEMY/Enemy.hpp
+++ SRC/NPC/ENTITI/ENEMY/Enemy.hpp
@@ -1,1 +1,16 @@
+#pragma once
+#include "SRC/NPC/ENTITI/ENTITI.hpp"
 
+class Enemy : public ENTITI{
+private:
+    char look;
+protected:
+    Enemy(std::string name,char look) : ENTITI(name) {
+        this->look = look;
+    }
+
+    char getLook(){
+        return this->look;
+    }
+public:
+};

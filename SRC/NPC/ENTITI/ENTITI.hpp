dfd6f9cd7b7d19f4172cfc41e3ba7694fd41e303
CMIK-slow <megabookZEQ@outlook.com>
2026-02-15 17:10:07 +0300

    Update ENTITI.hpp

--- SRC/NPC/ENTITI/ENTITI.hpp
+++ SRC/NPC/ENTITI/ENTITI.hpp
@@ -1,1 +1,16 @@
+#pragma once
+#include <string>
 
+class ENTITI{
+private:
+    std::string name;
+protected:
+    ENTITI(std::string name){
+        this->name = name;
+    }
+
+    std::string getName(){
+        return this->name;
+    }
+public:
+};

6f87eb9186d3c4a7c3460eab9aeac9fef1a8c499
CMIK-slow <megabookZEQ@outlook.com>
2026-02-15 17:11:03 +0300

    Update EnemyOne.hpp

--- SRC/NPC/ENTITI/ENEMY/EnemyONE/EnemyOne.hpp
+++ SRC/NPC/ENTITI/ENEMY/EnemyONE/EnemyOne.hpp
@@ -1,1 +1,14 @@
+#pragma once
+#include "SRC/NPC/ENTITI/ENEMY/Enemy.hpp"
+#include "SRC/SYSTEM/BYTE.hpp"
 
+class EnemyOne : public Enemy{
+private:
+    byte hp;
+    
+protected:
+    EnemyOne(std::string name,char look) : Enemy(name,look){
+    
+    }
+public:
+};
